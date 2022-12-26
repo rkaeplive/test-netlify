a23a9ec38cd91e694079e93f023421b98efb1b39
Роман <rkaeplive@gmail.com>
2022-12-26 12:24:02 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from "react";
-// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
-    // const [param, setParam] = useState([]);
-    // const getCustomParams = async () => {
-    //     const props = await axios
-    //         .get("http://localhost:8000/api/test", {
-    //             headers: {
-    //                 Authorization: localStorage.getItem("token"),
-    //             },
-    //         })
-    //         .catch((err) => {
-    //             console.log(err);
-    //         });
-    //     setParam(props.data);
-    // };
-    // useEffect(() => {
-    //     getCustomParams();
-    // }, []);git add .
-    // console.log(param);
-    const param = [1, 2, 3, 4, 5];
+    const [param, setParam] = useState([]);
+    const getCustomParams = async () => {
+        const props = await axios
+            .get("http://localhost:8000/api/test", {
+                headers: {
+                    Authorization: localStorage.getItem("token"),
+                },
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        setParam(props.data);
+    };
+    useEffect(() => {
+        getCustomParams();
+    }, []);
     return (
         <div className="App">
             {param.map((p) => (
