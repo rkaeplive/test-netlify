46260202b8530841e8e70daad1fe24421b168b57
Роман <rkaeplive@gmail.com>
2022-12-27 15:36:21 +0300

    1

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ function App() {
     const [param, setParam] = useState([]);
     const getCustomParams = async () => {
         const props = await axios
+            // https://test-netify-server.onrender.com
             .get("https://test-netify-server.onrender.com")
             .catch((err) => {
                 console.log(err);
             });
         setParam(props.data);
     };
+    console.log(param);
     useEffect(() => {
         getCustomParams();
     }, []);
     return (
         <div className="App">
             {param.map((p) => (
-                <div key={p}>{p}</div>
+                <div key={p.user_id}>{p.user_email}</div>
             ))}
         </div>
     );
